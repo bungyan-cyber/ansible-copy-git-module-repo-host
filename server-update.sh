018ed463137bc1c8806fff39fd06f186356ceef1
bungyan-cyber <dianpratama07@gmail.com>
2020-12-21 21:41:12 +0700

    updateshellscript

--- server-update.sh
+++ server-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ansible/fundemental/tech
 git clone --branch SLI0631 git ://git.labs.lod.netapp.com/ansible_labs_fundementals fundementals
-ansible-playbook /home/ansible/fundementals/playbooks.yml -e 'hostname-centos reponame=tech'
+ansible-playbook /home/ansible/fundementals/playbooks.yml -e hostname-centos reponame=tech
